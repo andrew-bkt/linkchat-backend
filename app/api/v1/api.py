f8f5572932a70b0779ed900db012f315cd633961
andrew-bkt <andrew@brooklyn-techology.com>
2024-09-17 00:59:44 -0400

    changes to chat

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -4,4 +4,3 @@ from app.api.v1.endpoints import users, chatbots, chat
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(chatbots.router, prefix="/chatbots", tags=["chatbots"])
-api_router.include_router(chat.router, prefix="/chatbots", tags=["chat"])  # Change this line
